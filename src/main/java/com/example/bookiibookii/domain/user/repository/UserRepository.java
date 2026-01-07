b3fda3405079aac3031fd075320ec6e4ba6d448f
kangyeson <yesonkang@gmail.com>
2026-01-07 14:40:37 +0900

    [Add] 제네릭 타입 파라미터 추가

--- src/main/java/com/example/bookiibookii/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/bookiibookii/domain/user/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.example.bookiibookii.domain.user.repository;
 
+import com.example.bookiibookii.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }

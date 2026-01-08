ef09051d12ee764d98e6e51319820619af4cf46b
kangyeson <yesonkang@gmail.com>
2026-01-08 20:39:25 +0900

    [Add] User 도메인, SocialType Enum 생성

--- src/main/java/com/example/bookiibookii/domain/user/entity/User.java
+++ src/main/java/com/example/bookiibookii/domain/user/entity/User.java
@@ -1,11 +1,11 @@
 package com.example.bookiibookii.domain.user.entity;
 
+import com.example.bookiibookii.domain.user.enums.SocialType;
 import com.example.bookiibookii.global.entity.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Builder
@@ -16,4 +16,18 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "social_type", nullable = false, length = 30)
+    private SocialType socialType;
+
+    @Column(name = "social_id", nullable = false)
+    private String socialId;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "manner", nullable = false)
+    @Builder.Default
+    private Double manner = 0.0;
 }

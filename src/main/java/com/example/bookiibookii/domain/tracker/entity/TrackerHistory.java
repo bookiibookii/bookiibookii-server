f95809554f3da57b572eb02683f23a7a860cb2ff
박성진 <106302098+macboy5@users.noreply.github.com>
2026-01-10 23:04:09 +0900

    feat : TrackerHistory Entity 초안

--- src/main/java/com/example/bookiibookii/domain/tracker/entity/TrackerHistory.java
+++ src/main/java/com/example/bookiibookii/domain/tracker/entity/TrackerHistory.java
@@ -1,5 +1,31 @@
 package com.example.bookiibookii.domain.tracker.entity;
 
-public class TrackerHistory {
+
+import com.example.bookiibookii.domain.tracker.enums.TrackerStatus;
+import com.example.bookiibookii.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TrackerHistory extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @OneToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "group_matching_id")
+//    private MatchedGroup matchedGroup;
+
+    @Enumerated(EnumType.STRING)
+    private TrackerStatus trackerStatus;
+
+    private String deliveryCompany;
+    private String trackingNumber;
+    private String imageUrl;
+
 
 }

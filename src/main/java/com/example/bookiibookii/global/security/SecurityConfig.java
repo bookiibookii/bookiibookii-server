f4fbe88612d11bb57b555a2735ffa1ca5a07c11f
gyeonseo <phsphg77@naver.com>
2026-01-15 01:42:50 +0900

    [Feat]#31 api test위한 엔드포인트 임시 인증 허가

--- src/main/java/com/example/bookiibookii/global/security/SecurityConfig.java
+++ src/main/java/com/example/bookiibookii/global/security/SecurityConfig.java
@@ -31,7 +31,9 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",        // 로그인, 토큰 재발급, 로그아웃
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/api/groups/**",
+                                "/api/test/**"// 로그인 구현 전 api 테스트 용도(나중에 삭제 필요)
                         ).permitAll()
                         .anyRequest().authenticated()
                 )

f5baa0d538da6b04951262ff2e20ad2b9c1fa991
taebin2 <109895271+taebin2@users.noreply.github.com>
2026-01-19 02:17:25 +0900

    [Feat]#28 유저북 엔티티 제작

---
+++ src/main/java/com/example/bookiibookii/domain/userbook/entity/UserBook.java
@@ -0,0 +1,38 @@
+package com.example.bookiibookii.domain.userbook.entity;
+
+import com.example.bookiibookii.domain.tracker.entity.Tracker;
+import com.example.bookiibookii.domain.user.entity.User;
+import com.example.bookiibookii.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user_book")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserBook extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_book_id")
+    private Long id;
+
+    @Column(name = "book_id", nullable = false)
+    private Long bookId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tracker_id", nullable = false)
+    private Tracker tracker;
+
+    @Column(name = "rating")
+    private Double rating;
+
+    @Column(name = "comment", length = 255)
+    private String comment;
+}

a8cf1572e8abf1c07d9ea661b5747ae96976e303
gyeonseo <phsphg77@naver.com>
2026-01-19 16:56:51 +0900

    [Feat]#60 Book 엔티티, 부키부키용 카테고리 enum 추가

---
+++ src/main/java/com/example/bookiibookii/domain/book/entity/Book.java
@@ -0,0 +1,38 @@
+package com.example.bookiibookii.domain.book.entity;
+
+import com.example.bookiibookii.domain.book.enums.CustomCategory;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "book")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private Long id;
+
+    @Column(name = "isbn", nullable = false)
+    private String isbn;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @Column(name = "publisher", nullable = false)
+    private String publisher;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false, length = 30)
+    private CustomCategory category;
+}

15da1070dc0dbf8db7f772d065a46807011fd73b
gyeonseo <phsphg77@naver.com>
2026-01-27 02:30:20 +0900

    [Feat]#90 알림 기본 타입 및 repository 생성

---
+++ src/main/java/com/example/bookiibookii/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.example.bookiibookii.domain.notification.repository;
+
+import com.example.bookiibookii.domain.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+}

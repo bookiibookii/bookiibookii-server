f533684d341689e8f774ae3d0d10a1d9a26db44a
gyeonseo <phsphg77@naver.com>
2026-01-27 02:26:50 +0900

    [Feat]#90 알림, 키워드 엔티티 생성

---
+++ src/main/java/com/example/bookiibookii/domain/notification/entity/Notification.java
@@ -0,0 +1,59 @@
+package com.example.bookiibookii.domain.notification.entity;
+
+import com.example.bookiibookii.domain.notification.enums.NotificationType;
+import com.example.bookiibookii.domain.user.entity.User;
+import com.example.bookiibookii.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notification")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Notification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long id;
+
+    // 수신자 - notNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_user_id", nullable = false)
+    private User receiver;
+
+    // 발송자 - 없는 알림도 존재(ex. 키워드알림)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "actor_user_id")
+    private User actor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "notification_type", nullable = false)
+    private NotificationType type;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    @Column(name = "payload", columnDefinition = "TEXT")
+    private String payload; // JSON string
+
+    @Column(name = "is_read", nullable = false)
+    boolean isRead;
+
+    @Column(name = "read_at")
+    private LocalDateTime readAt;
+
+    public void markAsRead() {
+        if (!this.isRead) {
+            this.isRead = true;
+            this.readAt = LocalDateTime.now();
+        }
+    }
+}

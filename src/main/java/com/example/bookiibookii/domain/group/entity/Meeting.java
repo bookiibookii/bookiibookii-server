a63482a39450e91553820b8d243c714413406896
박성진 <106302098+macboy5@users.noreply.github.com>
2026-01-27 19:28:31 +0900

    fix : meeting entity fix

--- src/main/java/com/example/bookiibookii/domain/group/entity/Meeting.java
+++ src/main/java/com/example/bookiibookii/domain/group/entity/Meeting.java
@@ -2,22 +2,32 @@ package com.example.bookiibookii.domain.group.entity;
 
 import com.example.bookiibookii.global.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Entity
+
+@Table(name = "meeting")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class Meeting extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "meeting_id")
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Groups group;
 
-    private LocalDateTime meetingTime;      // 교환희망일시
+
+    @Column(name = "meeting_time")
+    private LocalDateTime meetingTime;// 교환희망일시
+    @Column(name = "meeting_place")
     private String meetingPlace;            //교환희망장소
 
 }

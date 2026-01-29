839def2b2c4d9c902edcbbe2e3b6426841215071
taebin2 <109895271+taebin2@users.noreply.github.com>
2026-01-30 04:37:55 +0900

    [Feat]#119 프로필 사진 업로드 위한 DTO, ErrorCode세팅

--- src/main/java/com/example/bookiibookii/domain/user/exception/code/UserImageErrorCode.java
+++ src/main/java/com/example/bookiibookii/domain/user/exception/code/UserImageErrorCode.java
@@ -12,6 +12,8 @@ public enum UserImageErrorCode implements BaseCode {
     //400 BAD_REQUEST
     INVALID_S3_KEY_FORMAT(HttpStatus.BAD_REQUEST, "USERIMG400_1", "유효하지 않은 S3 키입니다. 형식: image/users/{userId}/{uuid}"),
     DUPLICATE_S3_KEY(HttpStatus.BAD_REQUEST, "USERIMG400_2", "이미 사용 중인 S3 키입니다."),
+    IMAGE_NOT_FOUND_IN_S3(HttpStatus.BAD_REQUEST, "USERIMG400_3", "S3에 해당 이미지가 존재하지 않습니다."),
+    S3_ACCESS_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "USERIMG500_1", "S3 접근 중 오류가 발생했습니다."),
 
     //404 NOT_FOUND
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USERIMG404_1", "해당 사용자를 찾을 수 없습니다."),

a0521df0df0dd86f05c5977eff69104fdbb1fe1b
gyeonseo <phsphg77@naver.com>
2026-01-30 12:06:30 +0900

    [Mod]#110 주입받는 변수명 통일

--- src/main/java/com/example/bookiibookii/domain/tracker/enums/TrackerNotiType.java
+++ src/main/java/com/example/bookiibookii/domain/tracker/enums/TrackerNotiType.java
@@ -6,12 +6,12 @@ public enum TrackerNotiType {
 
     READING_STARTED(
             "파트너가 책을 읽기 시작했어요!",
-            "{nick}님이 {bookTitle} 읽기를 시작했습니다."
+            "{nickname}님이 {bookTitle} 읽기를 시작했습니다."
     ),
 
     EXTEND_REQUESTED(
             "독서 기간이 조금 늘어났어요",
-            "{nick}님이 책을 더 깊이 읽기 위해 독서 기간을 연장했어요. (반납 예정일: {returnDueAt})"
+            "{nickname}님이 책을 더 깊이 읽기 위해 독서 기간을 연장했어요. (반납 예정일: {returnDueAt})"
     ),
 
     READING_FINISHED(
@@ -21,17 +21,17 @@ public enum TrackerNotiType {
 
     SHIPPING_REGISTERED(
             "소중한 책이 오고 있어요!",
-            "{nick}님이 책을 보내셨어요. 운송장 번호를 확인하고 배송을 조회해보세요."
+            "{nickname}님이 책을 보내셨어요. 운송장 번호를 확인하고 배송을 조회해보세요."
     ),
 
     RECEIVED_CONFIRMED(
             "책이 무사히 도착했대요!",
-            "{nick}님이 책을 잘 받으셨다고 해요. 수령 인증을 확인해볼까요?"
+            "{nickname}님이 책을 잘 받으셨다고 해요. 수령 인증을 확인해볼까요?"
     ),
 
     RETURN_SHIPPING_REGISTERED(
             "책이 집으로 돌아오고 있어요",
-            "{nick}님이 독서를 마치고 책을 반납했어요. 곧 소중한 책을 다시 만날 수 있어요."
+            "{nickname}님이 독서를 마치고 책을 반납했어요. 곧 소중한 책을 다시 만날 수 있어요."
     ),
 
     EXCHANGE_FINISHED(

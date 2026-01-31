08295904b77818ecc62aa4bb8ec5a724d19b75e6
kangyeson <yesonkang@gmail.com>
2026-01-31 16:00:41 +0900

    [Feat] 공지리스트 조회 API, 공지 상세 조회 API 구현

---
+++ src/main/java/com/example/bookiibookii/domain/support/entity/Notice.java
@@ -0,0 +1,35 @@
+package com.example.bookiibookii.domain.support.entity;
+
+import com.example.bookiibookii.domain.user.entity.User;
+import com.example.bookiibookii.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "notice")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Notice extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    // 작성자
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
+
+    @Column(name = "summary ", length = 255, nullable = false)
+    private String summary;
+
+    @Column(name = "content", length = 2000, nullable = false)
+    private String content;
+
+    @Column(name = "image")
+    private String image;
+}

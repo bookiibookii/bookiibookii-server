2e71cfc4d3629c0674f84cce97ba1be738ce0c88
kangyeson <yesonkang@gmail.com>
2026-02-02 04:17:27 +0900

    [Add] Address엔티티 추가

---
+++ src/main/java/com/example/bookiibookii/domain/user/repository/AddressRepository.java
@@ -0,0 +1,12 @@
+package com.example.bookiibookii.domain.user.repository;
+
+import com.example.bookiibookii.domain.user.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+    Optional<Address> findByUserId(Long userId);
+}

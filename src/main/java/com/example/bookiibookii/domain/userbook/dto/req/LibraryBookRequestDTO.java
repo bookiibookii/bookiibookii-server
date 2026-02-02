2cb7fdc016a094cb42d12070e55b769e79d7f50a
sjinssun <seokjinsun@naver.com>
2026-02-02 21:45:46 +0900

    [Mod] @Setter어노테이션 추가

--- src/main/java/com/example/bookiibookii/domain/userbook/dto/req/LibraryBookRequestDTO.java
+++ src/main/java/com/example/bookiibookii/domain/userbook/dto/req/LibraryBookRequestDTO.java
@@ -2,12 +2,14 @@ package com.example.bookiibookii.domain.userbook.dto.req;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 public class LibraryBookRequestDTO {
 
     @Getter
     @NoArgsConstructor
+    @Setter
     public static class SearchDTO {
         private String keyword; // 검색어 (제목, 저자, 한줄평)
     }

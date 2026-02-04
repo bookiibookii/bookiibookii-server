419384056d4e4627e40870efc3e14350bd09f11d
gyeonseo <phsphg77@naver.com>
2026-02-05 05:16:15 +0900

    [Fix]#178 redisConfig - boot 자동설정으로 변경

--- src/main/java/com/example/bookiibookii/global/security/RedisConfig.java
+++ src/main/java/com/example/bookiibookii/global/security/RedisConfig.java
@@ -1,34 +1,19 @@
 package com.example.bookiibookii.global.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-
-    // <String, Object>가 아니라 <String, String>으로 사용 (직접 JSON 변환 후 넣을 것이므로)
-    @Bean
-    public RedisTemplate<String, String> redisTemplate() {
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(cf);
 
-        // Key, Value, HashKey, HashValue 모두 문자열로 저장
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());

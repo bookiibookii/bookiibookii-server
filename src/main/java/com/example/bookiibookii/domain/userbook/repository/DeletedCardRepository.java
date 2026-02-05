a1ae2968aafc3ad697eefe02f3db03378ae370c0
taebin2 <109895271+taebin2@users.noreply.github.com>
2026-02-06 02:39:48 +0900

    [Feat]#228 삭제된 카드에 대한 정보를 담고 있을 deletedCard 엔티티 제작 및 repository 제작

---
+++ src/main/java/com/example/bookiibookii/domain/userbook/repository/DeletedCardRepository.java
@@ -0,0 +1,21 @@
+package com.example.bookiibookii.domain.userbook.repository;
+
+import com.example.bookiibookii.domain.userbook.entity.DeletedCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface DeletedCardRepository extends JpaRepository<DeletedCard, Long> {
+
+    boolean existsByUser_IdAndCard_Id(Long userId, Long cardId);
+
+    Optional<DeletedCard> findByUser_IdAndCard_Id(Long userId, Long cardId);
+
+    @Query("SELECT dc.card.id FROM DeletedCard dc WHERE dc.user.id = :userId")
+    List<Long> findCardIdsByUser_Id(@Param("userId") Long userId);
+}

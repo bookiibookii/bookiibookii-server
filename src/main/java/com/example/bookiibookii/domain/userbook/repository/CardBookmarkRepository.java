1442c8ecc6c3e5e2134a88c5008bf2668ebaa6e9
taebin2 <109895271+taebin2@users.noreply.github.com>
2026-02-05 19:19:05 +0900

    [Feat]#225 CardBookmarkRepository를 추가하였습니다. 쿼리문이 조금 복잡해요,,

---
+++ src/main/java/com/example/bookiibookii/domain/userbook/repository/CardBookmarkRepository.java
@@ -0,0 +1,21 @@
+package com.example.bookiibookii.domain.userbook.repository;
+
+import com.example.bookiibookii.domain.userbook.entity.CardBookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CardBookmarkRepository extends JpaRepository<CardBookmark, Long> {
+
+    boolean existsByUser_IdAndCard_Id(Long userId, Long cardId);
+
+    Optional<CardBookmark> findByUser_IdAndCard_Id(Long userId, Long cardId);
+
+    @Query("SELECT cb FROM CardBookmark cb JOIN FETCH cb.card c JOIN FETCH c.cardImage JOIN FETCH c.userBook ub JOIN FETCH ub.group g JOIN FETCH g.book WHERE cb.user.id = :userId ORDER BY cb.createdAt DESC")
+    List<CardBookmark> findByUser_IdWithCardAndImageAndBookOrderByCreatedAtDesc(@Param("userId") Long userId);
+}

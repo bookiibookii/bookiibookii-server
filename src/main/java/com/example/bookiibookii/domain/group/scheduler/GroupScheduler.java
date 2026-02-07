08937ad9f4698cdf3b53303cf6a53abaf2831f72
gyeonseo <phsphg77@naver.com>
2026-02-07 23:47:50 +0900

    [Mod]#261 알람 발행 조건문 안으로 변경

--- src/main/java/com/example/bookiibookii/domain/group/scheduler/GroupScheduler.java
+++ src/main/java/com/example/bookiibookii/domain/group/scheduler/GroupScheduler.java
@@ -50,15 +50,20 @@ public class GroupScheduler {
                         group.getStartDate(),
                         group.getMaxCapacity()
                 ));
+                // 매칭 성공 알람
+//                eventPublisher.publish(new GroupNotificationEvent(
+//                        MATCH_SUCCEEDED, group.getHost().getId(), group.getBook().getTitle(),
+//                        newMember.getUser().getId(), null, group.getGroupId()
+//                ));
+                // 대기자들 거절 알람
 
             } else {
                 group.markAsDELETED();
                 eventPublisher.publish(new GroupNotificationEvent(MATCH_EXPIRED, null, group.getBook().getTitle(), group.getHost().getId(), null, group.getGroupId()));
+                // 신청자 관리: 알림 발송을 위해 대기자 명단 먼저 추출
+                List<Long> rcvIds = applicationRepository.findPendingUserIdsByGroupId(group.getGroupId());
+                eventPublisher.publish(new GroupNotificationEvent(MATCH_AUTO_REJECTED, group.getHost().getId(), group.getBook().getTitle(), null, rcvIds, group.getGroupId()));
             }
-
-            // 신청자 관리: 알림 발송을 위해 대기자 명단 먼저 추출
-            List<Long> rcvIds = applicationRepository.findPendingUserIdsByGroupId(group.getGroupId());
-            eventPublisher.publish(new GroupNotificationEvent(MATCH_AUTO_REJECTED,  group.getHost().getId(), group.getBook().getTitle(), null, rcvIds, group.getGroupId()));
 
             // 어떤 경우든 남은 대기자(Pending)는 일괄 거절 처리
             applicationRepository.updatePendingToRejectedByGroupId(group.getGroupId(), ApplicationStatus.REJECTED);

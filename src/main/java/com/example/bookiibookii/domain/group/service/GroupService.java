c1af8522b99d2cedf8a6d310a265553e5e344cbf
박성진 <106302098+macboy5@users.noreply.github.com>
2026-02-10 20:12:59 +0900

    mod : meeting 레코드 생성 분기

--- src/main/java/com/example/bookiibookii/domain/group/service/GroupService.java
+++ src/main/java/com/example/bookiibookii/domain/group/service/GroupService.java
@@ -149,15 +149,24 @@ public class GroupService {
         // 1:1 직접 교환일 때 Meeting 초기 데이터 생성
         if (request.getGroupType() == GroupType.RELAY && request.getTradeType() == TradeType.DIRECT) {
 
-            // request.getMeetPlace()를 통해 유저가 최종 수정한 주소를 저장
-            Meeting initialMeeting = Meeting.builder()
-                    .group(savedGroup)
-                    .meetingPlace(request.getMeetPlace()) //host 프로필이 아닌 DTO 값 사용
-                    .trackerStatus(TrackerStatus.READY)
+            // 1. 전달용 약속 (호스트 -> 게스트)
+            Meeting shippingMeeting = Meeting.builder()
+                    .group(group)
+                    .trackerStatus(TrackerStatus.SHIPPING_TO_GUEST)
+                    .meetingPlace(group.getHost().getMeetPlace())
                     .meetingTime(null)
                     .build();
 
-            meetingRepository.save(initialMeeting);
+            // 2. 반납용 약속 (마지막 게스트 -> 호스트)
+            Meeting returnMeeting = Meeting.builder()
+                    .group(group)
+                    .trackerStatus(TrackerStatus.SHIPPING_TO_HOST)
+                    .meetingPlace(group.getHost().getMeetPlace())
+                    .meetingTime(null)
+                    .build();
+
+            meetingRepository.saveAll(List.of(shippingMeeting, returnMeeting));
+
         }
 
         // 방장을 MatchedMember의 첫 번째 멤버로 등록

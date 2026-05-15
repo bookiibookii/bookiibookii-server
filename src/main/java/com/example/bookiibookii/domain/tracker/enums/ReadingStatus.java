fe37e94e51d95142ce636f0248fee8596e8dd265
gyeonseo <gyeon8190@gachon.ac.kr>
2026-05-16 03:04:12 +0900

    [Refactor]#446 교환 단계 설명 추가

--- src/main/java/com/example/bookiibookii/domain/tracker/enums/ReadingStatus.java
+++ src/main/java/com/example/bookiibookii/domain/tracker/enums/ReadingStatus.java
@@ -9,11 +9,11 @@ public enum ReadingStatus {
 
     MY_BOOK_READING("자신의 책 읽는 중"),
     MY_BOOK_REVIEWING("1차 독서 완료, 후기 작성 중"),
-    EXCHANGING("1차 교환 진행 중"),
+    EXCHANGING("자신의 책 후기 작성 완료, 1차 교환 진행 중"),
     EXCHANGED("1차 교환 완료"),
     PARTNER_BOOK_READING("파트너 책 읽는 중"),
     PARTNER_BOOK_REVIEWING("2차 독서 완료, 후기 작성 중"),
-    RETURNING("2차 교환(반납) 진행 중"),
+    RETURNING("파트너 책 후기 작성 완료, 2차 교환(반납) 진행 중"),
     COMPLETED("릴레이 종료");
 
     private final String description;

9d9d5802c76a52424e6b1468992d52d1070a0a86
kangyeson <yesonkang@gmail.com>
2026-05-15 18:01:18 +0900

    [Chore] Response 설명 수정

--- src/main/java/com/example/bookiibookii/domain/location/dto/req/UserDeliveryReqDTO.java
+++ src/main/java/com/example/bookiibookii/domain/location/dto/req/UserDeliveryReqDTO.java
@@ -17,10 +17,10 @@ public class UserDeliveryReqDTO {
 
             String addressDetail,
 
-            @NotBlank(message = "받는 사람은 필수입니다.")
+            @NotBlank(message = "배송지 등록을 위해 수취인 이름은 필수로 입력되어야 합니다.")
             String receiverName,
 
-            @NotBlank(message = "전화번호는 필수입니다.")
+            @NotBlank(message = "배송지 등록을 위해 전화번호는 필수로 입력되어야 합니다.")
             @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "전화번호 형식이 올바르지 않습니다.")
             String phone
     ) {}

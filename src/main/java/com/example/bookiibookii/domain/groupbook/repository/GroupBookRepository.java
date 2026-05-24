a1de843f8d0d1ba60808d21bc84cb2ef2581135a
taebin2 <109895271+taebin2@users.noreply.github.com>
2026-05-24 16:06:48 +0900

    [Delete] tracker-groupbook 의존성 제거

--- src/main/java/com/example/bookiibookii/domain/groupbook/repository/GroupBookRepository.java
+++ src/main/java/com/example/bookiibookii/domain/groupbook/repository/GroupBookRepository.java
@@ -4,10 +4,6 @@ import com.example.bookiibookii.domain.groupbook.entity.GroupBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface GroupBookRepository extends JpaRepository<GroupBook, Long> {
-
-    List<GroupBook> findAllByGroup_Id(Long groupId);
 }

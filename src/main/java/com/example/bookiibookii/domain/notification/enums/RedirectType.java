fe49bbba4a01b4d8e32932becd92f66edfd35a7b
gyeonseo <gyeon8190@gachon.ac.kr>
2026-06-10 14:09:54 +0900

    [Refactor]#549 리다이렉트 위한 payload 필드 추가

---
+++ src/main/java/com/example/bookiibookii/domain/notification/enums/RedirectType.java
@@ -0,0 +1,12 @@
+package com.example.bookiibookii.domain.notification.enums;
+
+public enum RedirectType {
+    APPLICATION_MANAGEMENT,
+    EXPLORE_HOME,
+    GROUP_DETAIL,
+    TRACKER_HOME,
+    TRACKER_DETAIL,
+    TRACKER_COMMENT,
+    BOOK_CARD_DETAIL,
+    NOTICE_DETAIL
+}

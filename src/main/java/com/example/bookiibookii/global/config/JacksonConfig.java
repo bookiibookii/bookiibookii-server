6399acd7cc5962f78797fe74923cfe0c9f0ff906
gyeonseo <gyeon8190@gachon.ac.kr>
2026-06-24 13:50:53 +0900

    [Refactor]#580 utc 기반 시간 설정 추가

--- src/main/java/com/example/bookiibookii/global/config/JacksonConfig.java
+++ src/main/java/com/example/bookiibookii/global/config/JacksonConfig.java
@@ -1,14 +1,20 @@
 package com.example.bookiibookii.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return JsonMapper.builder()
+                .findAndAddModules()
+                .defaultTimeZone(TimeZone.getTimeZone("UTC"))
+                .build();
     }
 }
